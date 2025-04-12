32b3ab674ec12181ec1a325cedae0e16d10bf307
shounza <smohamadazni@gmail.com>
2025-04-13 06:25:08 +0700

    Add gold-trader/backend/database/database_setup.py 1744500354754

--- gold-trader/backend/database/database_setup.py
+++ gold-trader/backend/database/database_setup.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.exc import SQLAlchemyError
-from models import Base
+from database.models import Base
 from config import Config
 from logger import logger
 
